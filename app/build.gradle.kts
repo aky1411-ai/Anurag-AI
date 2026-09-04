70f08366251c4d6c0bc0a4f9e6755b30a58861c8
aky1411-ai <abcdeabcde1234567890987654321@gmail.com>
2026-09-05 01:26:47 +0530

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "com.anuragai.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.anuragai.app"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
+    implementation(platform("androidx.compose:compose-bom:2025.01.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+}
